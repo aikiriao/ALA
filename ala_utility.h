225fdbdec9248690a4dd9fe55fb224b65b812bae
Masatomo Aiki <vim_penjp_doujin_lover@ezweb.ne.jp>
2019-08-22 22:33:56 +0900

    Delete unused macros.

--- ala_utility.h
+++ ala_utility.h
@@ -15,26 +15,19 @@
 #else
 /* 算術右シフトが無効な環境では、自分で定義する ハッカーのたのしみのより引用 */
 /* 注意）有効範囲:0 <= rshift <= 32 */
-#define ALAUTILITY_SHIFT_RIGHT_ARITHMETIC(sint32, rshift) ((((uint64_t)(sint32) + 0x80000000UL) >> (rshift)) - (0x80000000UL >> (rshift)))
+#define ALAUTILITY_SHIFT_RIGHT_ARITHMETIC(sint32, rshift) \
+  ((((uint64_t)(sint32) + 0x80000000UL) >> (rshift)) - (0x80000000UL >> (rshift)))
 #endif
-/* 符号関数 ハッカーのたのしみより引用 補足）val==0の時は0を返す */
-#define ALAUTILITY_SIGN(val)  (int32_t)((-(((uint32_t)(val)) >> 31)) | (((uint32_t)-(val)) >> 31))
 /* 最大値の取得 */
 #define ALAUTILITY_MAX(a,b) (((a) > (b)) ? (a) : (b))
 /* 最小値の取得 */
 #define ALAUTILITY_MIN(a,b) (((a) < (b)) ? (a) : (b))
 /* 最小値以上最小値以下に制限 */
 #define ALAUTILITY_INNER_VALUE(val, min, max) (ALAUTILITY_MIN((max), ALAUTILITY_MAX((min), (val))))
-/* 2の冪乗か？ */
-#define ALAUTILITY_IS_POWERED_OF_2(val) (!((val) & ((val) - 1)))
 /* 符号付き32bit数値を符号なし32bit数値に一意変換 */
 #define ALAUTILITY_SINT32_TO_UINT32(sint) (((int32_t)(sint) < 0) ? ((uint32_t)((-((sint) << 1)) - 1)) : ((uint32_t)(((sint) << 1))))
 /* 符号なし32bit数値を符号付き32bit数値に一意変換 */
 #define ALAUTILITY_UINT32_TO_SINT32(uint) ((int32_t)((uint) >> 1) ^ -(int32_t)((uint) & 1))
-/* 絶対値の取得 */
-#define ALAUTILITY_ABS(val)               (((val) > 0) ? (val) : -(val))
-/* 32bit整数演算のための右シフト量を計算 */
-#define ALAUTILITY_CALC_RSHIFT_FOR_SINT32(bitwidth) (((bitwidth) > 16) ? ((bitwidth) - 16) : 0)
 
 #ifdef __cplusplus
 extern "C" {
